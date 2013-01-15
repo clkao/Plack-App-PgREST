d68f1162474e456bc6bd81f3e539703363aecda3
Chia-liang Kao <clkao@clkao.org>
2013-01-15 18:30:18 +0800

    support q:{} and q: {$: {}}

--- lib/Plack/App/PgREST.pm
+++ lib/Plack/App/PgREST.pm
@@ -7,7 +7,7 @@ use Router::Resource;
 use parent qw(Plack::Component);
 use Plack::Util::Accessor qw( dsn dbh pg_version );
 use Plack::Request;
-use JSON qw(encode_json);
+use JSON qw(encode_json decode_json);
 use IPC::Run3 qw(run3);
 
 sub n {
@@ -24,6 +24,7 @@ method select($param, $args) {
         l => n($param->get('l')),
         sk => n($param->get('sk')),
         c => n($param->get('c')),
+        q => decode_json($param->get('q') || undef),
     });
     my $ary_ref = $self->{dbh}->selectall_arrayref("select postgrest_select(?)", {}, $req);
     if (my $callback = $param->get('callback')) {
@@ -134,8 +135,74 @@ END
 END
 
     $self->{dbh}->do($self->_mk_func("postgrest_select", [req => "pgrest_json"], "pgrest_json", << 'END', 'plls'));
+
+q = -> """
+    '#{ "#it".replace /'/g "''" }'
+"""
+qq = -> """
+    "#{ "#it".replace /"/g '""' }"
+"""
+
+walk = (model, meta) ->
+    return [] unless meta?[model]
+    for col, spec of meta[model]
+        [compile(model, spec), col]
+
+compile = (model, field) ->
+    {$query, $from, $and, $} = field ? {}
+    switch
+    | $from? => """
+        (SELECT COALESCE(ARRAY_TO_JSON(ARRAY_AGG(_)), '[]') FROM (SELECT * FROM #from-table
+            WHERE #{ qq "_#model" } = #model-table."_id" AND #{
+                switch
+                | $query?                   => cond model, $query
+                | _                         => true
+            }
+        ) AS _)
+    """ where from-table = qq "#{$from}s", model-table = qq "#{model}s"
+    | $? => cond model, $
+    | typeof field is \object => cond model, field
+    | _ => field
+
+cond = (model, spec) -> switch typeof spec
+    | \number => spec
+    | \string => qq spec
+    | \object =>
+        # Implicit AND on all k,v
+        [ test model, qq(k), v for k, v of spec ] * "AND"
+    | _ => it
+
+test = (model, key, expr) -> switch typeof expr
+    | <[ number boolean ]> => "(#key = #expr)"
+    | \string => "(#key = #{ q expr })"
+    | \object => for op, ref of expr
+        switch op
+            | \$gt =>
+                res = evaluate model, ref
+                return "(#key > #res)"
+            | \$ =>
+                return "#key = #model-table.#{ qq ref }" where model-table = qq "#{model}s"
+            | _ => throw "Unknown operator: #op"
+    | \undefined => true
+
+evaluate = (model, ref) -> switch typeof ref
+    | <[ number boolean ]> => "#ref"
+    | \string => q #ref
+    | \object => for op, v of ref => switch op
+        | \$ => "#model-table.#{ qq v }" where model-table = qq "#{model}s"
+        | \$ago => "'now'::timestamptz - #{ q "#v ms" }::interval"
+        | _ => continue
+
+#module.exports = exports = { walk, compile }
+
+
+
 ({collection, l = 30, sk = 0, q, c, q, fo}) ->
+    var cond
+    if q
+        cond = compile collection, q
     query = "select * from #collection"
+    query += " WHERE #cond" if cond?
     [{count}] = plv8.execute "select count(*) from (#query) cnt"
     return { count } if c
 
