e92c882015cdf2867525e07aa9e5a5e3a7cf9ede
Chia-liang Kao <clkao@clkao.org>
2013-01-18 14:31:09 +0800

    Checking in changes prior to tagging of version 0.02.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 0d76310..370b07c 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,4 +1,7 @@
     Revision history for Perl extension Plack::App::PgREST
    
    +0.02  Mon Jan 18 14:30:01 2013
    +        - dist cleanups
    +
     0.01  Mon Jan  7 17:18:01 2013
             - original version

--- lib/Plack/App/PgREST.pm
+++ lib/Plack/App/PgREST.pm
@@ -1,7 +1,7 @@
 package Plack::App::PgREST;
 use methods;
 use 5.008_001;
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 use Plack::Builder;
 use Router::Resource;
 use parent qw(Plack::Component);
